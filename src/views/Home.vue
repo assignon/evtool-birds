2a521d29f8cb18957d675d7fb32bb8463d10d0a2
Yanick Assignon <assignon>
2021-10-22 10:59:03 +0200

    project setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,47 @@
 <template>
   <div class="home">
+    <mapbox
+      :access-token='accessToken'
+      :map-options="{
+        style: 'mapbox://styles/mapbox/light-v9',
+        center: [-96, 37.8],
+        zoom: 3,
+      }"
+      :geolocate-control="{
+        show: true,
+        position: 'top-left',
+      }"
+      @map-load="loaded"
+      @map-zoomend="zoomend"
+      @map-click:points="clicked"
+      @geolocate-error="geolocateError"
+      @geolocate-geolocate="geolocate"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
+import { mapGetters} from 'vuex'
+import Mapbox from 'mapbox-gl-vue'
 
 export default {
   name: "Home",
 
   components: {
+    Mapbox
+  },
+
+  computed: {
+    ...mapGetters({
+
+    })
   },
 
   data() {
     return {
-
+      accessToken: process.env.MAPBOX_ACCES_TOKEN
     }
   },
 
@@ -41,3 +67,7 @@ export default {
 
 };
 </script>
+
+<style lang="" scoped>
+  
+</style>
